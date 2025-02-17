c35ec0661fc76de9dd1aac25713afec250465fe8
yushi-yamamoto-fd <yushi.yamamoto@fdteam.jp>
2025-02-17 20:25:04 +0900

    0.03

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next"
 import { GlobalStateProvider } from "src/contexts/GlobalStateContext"
 
 export const metadata: Metadata = {
-    title: 'FullstackLab.net | Dennis-Immanuel Czogalla - Fullstack Developer',
+    title: 'YUSHI YAMAMOTO | Portfolio',
     description: "デザイン、プログラミング、マーケティング、そしてAI技術まで、多岐にわたる分野で活動しているマルチプレイヤー型クリエイターです。これまでのキャリアを通じて、**「エンジニア」「マーケター」「デザイナー」「プロデューサー」**として、それぞれの視点から価値を生み出すことに挑戦してきました。",
 }
 
@@ -12,7 +12,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="ja">
             <body>
                 <GlobalStateProvider >
                     <div id="root">{children}</div>
