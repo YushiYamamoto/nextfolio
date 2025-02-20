3f5dc615df437c81e0c58ae9e75dc06e9ee0f11f
YUSHI YAMAMOTO <32246139+YushiYamamoto@users.noreply.github.com>
2025-02-20 14:31:05 +0900

    サービスの変更

--- src/components/layout/Services.tsx
+++ src/components/layout/Services.tsx
@@ -7,9 +7,9 @@ export default function Services() {
     <img className="w-full h-44" src="/transitions/transition_gradient.svg" alt="transition" />
     <p className="text-4xl mb-8 mt-8  text-white font-bold">サービス</p>
     <div className="flex desktop:flex-row mobile:flex-col flex-wrap mobile:items-center pb-8 justify-center text-center h-full">
-      <Service hl="シングルページアプリケーション" desc="ポートフォリオ、ランディングページ、ダッシュボードなどのレスポンシブWebアプリの開発" img="/icons/SPA_Icon.svg" />
-      <Service hl="API連携" desc="ダイナミックなコンテンツ配信のためのシームレスな統合" img="/icons/API_Icon.svg" />
-      <Service hl="SEO最適化" desc="Webアプリの視認性と検索順位を向上" img="/icons/SEO_Icon.svg" />
+      <Service hl="インタラクティブWebサイト制作" desc="Three.jsやGSAPを活用し、動きのある高品質なWebサイトを制作" img="/icons/SPA_Icon.svg" />
+      <Service hl="カスタムWebアプリ開発" desc="ReactやVueを使ったオーダーメイドのWebアプリを構築" img="/icons/API_Icon.svg" />
+      <Service hl="動画×Web融合サービス" desc="動画制作とWebデザインを組み合わせたプロモーションサイト制作" img="/icons/SEO_Icon.svg" />
     </div>
     <p className="desktop:text-[40px] mobile:text-[24px] font-bold my-4 text-center">お気軽にご連絡ください</p>
     <ContactBtn title={"お問い合わせ"} />
