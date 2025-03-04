fd625a9db18673855b2fb7e45d6c42e3636980ae
YushiYamamoto <u.seek.your.ship@gmail.com>
2025-03-04 22:09:25 +0900

    らくらくサイトを追加

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja', 'en'],
+  },
+  localePath: typeof window === 'undefined'
+    ? require('path').resolve('./public/locales')
+    : '/locales',
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
+import nextI18nConfig from './next-i18next.config.js';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: 'build', // Changes the build output directory to `./dist/`.
-    images: {
-      unoptimized: true,
-    },
-    reactStrictMode: false
-  }
-   
-  export default nextConfig
+  // output: 'export', // Outputs a Single-Page Application (SPA).
+  distDir: 'build', // Changes the build output directory to `./dist/`.
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  ...nextI18nConfig.i18n,
+}
+
+export default nextConfig
