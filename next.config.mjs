8babc04a52375700c4e189c41b5a10903b78a8f5
YushiYamamoto <u.seek.your.ship@gmail.com>
2025-03-04 23:59:00 +0900

    gonyo gonyo 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
+[build]
+  base = "/"
+  publish = "nextfolio/.next"
+  command = "npm run build"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // Outputs a Single-Page Application (SPA).
-  // output: 'export',
-  distDir: 'build', // Changes the build output directory to `./dist/`.
-  images: {
-    unoptimized: true,
-  },
-  reactStrictMode: true,
+  distDir: '.next', // 出力ディレクトリを明示的に指定
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja', 'en'],
   },
-}
+  // Uncomment if you want to export as a static site
+  // output: 'export',
+};
 
-export default nextConfig
+export default nextConfig;
